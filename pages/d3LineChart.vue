d1055e008b065c1aba15ceb3b5a66eb2a1f3b1b3
LAPTOP-MJ27P059\kjy93 <ns9337@naver.com>
2022-05-03 10:49:48 +0900

    feat : d3 라이브러리(lineChart,RadialCluster 등) 예제 적용

---
+++ pages/d3LineChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg ref="line"></svg>
+</template>
+
+<script>
+import * as d3 from 'd3'
+export default {
+  data() {
+    return {
+      data: [90, 72, 75, 25, 10, 92],
+    }
+  },
+  computed: {
+    line() {
+      return d3
+        .line()
+        .x((d, i) => this.xScale(i))
+        .y((d) => this.ySclae(d))
+    },
+    xScale() {
+      return d3
+        .scaleLinear()
+        .range([20, 480])
+        .domain(d3.extent(this.data, (d, i) => i))
+    },
+    ySclae() {
+      return d3.scaleLinear().range([280, 20]).domain([0, 100])
+    },
+  },
+  mounted() {
+    const svg = d3
+      .select(this.$refs.line)
+      .attr('width', 500)
+      .attr('height', 300)
+
+    svg
+      .append('path')
+      .datum(this.data)
+      .attr('fill', 'none')
+      .attr('stroke', '#76BF8A')
+      .attr('stroke-width', 3)
+      .attr('d', this.line)
+  },
+}
+</script>
+
+<style>
+</style>
