daefe4933872de54453b545bcf42c5f9b945f525
LAPTOP-MJ27P059\kjy93 <ns9337@naver.com>
2022-05-03 10:55:41 +0900

    feat : 로그인 api url 변경

--- util/TempAdminApiMap.js
+++ util/TempAdminApiMap.js
@@ -1,12 +1,12 @@
 export const EndPoint = {
-  // 관리자 로그인
-  POST_ADMIN_LOGIN: `http://localhost:9001/admins/login`,
+  // 유저 로그인
+  POST_USER_LOGIN: `http://localhost:9000/user/login`,
 }
 
 const TempAdminApiMap = {
   get: {},
   post: {
-    [EndPoint.POST_ADMIN_LOGIN]: {},
+    [EndPoint.POST_USER_LOGIN]: {},
   },
   patch: {},
   put: {},
